b5999f964a23b05d9c6a3da5a0b4e73ced94d6a3
lizhaohua <lichaohua@.mocentre.com>
2019-03-05 09:51:32 +0800

    up

--- docs/js/index.js
+++ docs/js/index.js
@@ -31,20 +31,23 @@ var addClass = function(element, className) {
 
 // 打字效果
 var arr = [
-    { type: 'text', text: '某某某，又名某某某，是个十足的女神经，嗯？' },
+    { type: 'text', text: 'W-L-J，又名万万ya，是个十足的美女吧，嗯？' },
     { type: 'wait', time: 900 },
     { type: 'delete', num: 4 },
     { type: 'text', text: '，嗯!' },
     { type: 'br' },
     { type: 'text', text: '她温柔美丽，善良大方' },
-    { type: 'text', text: '，国色天香，沉鱼落雁，如花似玉，闭月羞花，贤良淑德，花容月貌，秋水伊人，一笑倾城，冰清玉洁，娇俏佳人，朱颜玉润，玉骨冰肌，窈窕淑女，美若天仙，一顾倾城，才智国人，出水芙蓉，阿娇金屋，闭月羞花，逞娇呈美，春暖花香，春色满园……', time: 50 },
+    { type: 'text', text: '，国色天香，沉鱼落雁，如花似玉，闭月羞花，贤良淑德，花容月貌，秋水伊人，一笑倾城，冰清玉洁，娇俏佳人，朱颜玉润，玉骨冰肌，窈窕淑女，美若天仙，一顾倾城，倾国倾城，温婉娴淑，千娇百媚，仪态万千，花容月貌，明目皓齿，清艳脱俗，香肌玉肤，仪态万端，婉风流转，美撼凡尘，聘婷秀雅，娥娜翩跹，俏丽多姿，顾盼流转，举步轻摇，艳冠群芳，才智国人，出水芙蓉，阿娇金屋，闭月羞花，逞娇呈美，春暖花香，春色满园……', time: 50 },
     { type: 'br' },
-    { type: 'text', text: '给你们看张她的照片吧~' },
+    { type: 'text', text: '给你们看张我们的照片吧~' },
     { type: 'wait', time: 900 },
-    { type: 'img', src: 'img/bishi.jpg', id: "cat", style: "width: 50%;display: block;margin-left: auto;margin-right: auto;margin-top: 20px;margin-bottom: 20px;" },
+    { type: 'img', src: 'img/love.jpg', id: "cat", style: "width: 50%;display: block;margin-left: auto;margin-right: auto;margin-top: 20px;margin-bottom: 20px;" },
     { type: 'wait', time: 900 },
     // { type: 'delete', num: 1, time: 1000 },
-    { type: 'text', text: '是不是美美哒~' }
+    { type: 'text', text: '她是不是美美哒~' },
+    { type: 'br' },
+    { type: 'wait', time: 900 },
+    { type: 'text', text: '不要生气啦~ /抱抱' },
 ]
 
 var autoType = new AutoType(content, arr);
